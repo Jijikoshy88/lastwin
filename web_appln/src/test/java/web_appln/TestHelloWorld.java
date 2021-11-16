6823230d8a4fdcb2e211d0e46b75eba9a924b35d
Jijikoshy88 <81867861+Jijikoshy88@users.noreply.github.com>
2021-11-16 18:04:59 +0530

    Add files via upload

---
+++ web_appln/src/test/java/web_appln/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package web_appln;
+
+public class TestHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi hello world");
+
+	}
+
+}
